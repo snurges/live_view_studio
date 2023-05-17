1c4f3d35fc66d95eea9ab20da5c18a8a21f98579
Siim Nurges <siim.nurges@yolo.com>
2023-05-17 10:01:07 +0300

    autocomplete

--- lib/live_view_studio_web/live/flights_live.ex
+++ lib/live_view_studio_web/live/flights_live.ex
@@ -1,11 +1,17 @@
 defmodule LiveViewStudioWeb.FlightsLive do
+  alias LiveViewStudio.Flights
   use LiveViewStudioWeb, :live_view
+
+  alias LiveViewStudio.Flights
+  alias LiveViewStudio.Airports
 
   def mount(_params, _session, socket) do
     socket =
       assign(socket,
         airport: "",
-        flights: []
+        flights: [],
+        loading: false,
+        matches: %{}
       )
 
     {:ok, socket}
@@ -15,7 +21,7 @@ defmodule LiveViewStudioWeb.FlightsLive do
     ~H"""
     <h1>Find a Flight</h1>
     <div id="flights">
-      <form>
+      <form phx-submit="search" phx-change="suggest">
         <input
           type="text"
           name="airport"
@@ -23,6 +29,9 @@ defmodule LiveViewStudioWeb.FlightsLive do
           placeholder="Airport Code"
           autofocus
           autocomplete="off"
+          readonly={@loading}
+          list="matches"
+          phx-debounce="1000"
         />
 
         <button>
@@ -30,31 +39,66 @@ defmodule LiveViewStudioWeb.FlightsLive do
         </button>
       </form>
 
+      <datalist id="matches">
+        <option :for={{code, name} <- @matches} value={code}>
+          <%= name %>
+        </option>
+      </datalist>
+
+      <div :if={@loading} class="loader">Loading...</div>
+
       <div class="flights">
         <ul>
-          <%= for flight <- @flights do %>
-            <li>
-              <div class="first-line">
-                <div class="number">
-                  Flight #<%= flight.number %>
-                </div>
-                <div class="origin-destination">
-                  <%= flight.origin %> to <%= flight.destination %>
-                </div>
+          <li :for={flight <- @flights}>
+            <div class="first-line">
+              <div class="number">
+                Flight #<%= flight.number %>
               </div>
-              <div class="second-line">
-                <div class="departs">
-                  Departs: <%= flight.departure_time %>
-                </div>
-                <div class="arrives">
-                  Arrives: <%= flight.arrival_time %>
-                </div>
+              <div class="origin-destination">
+                <%= flight.origin %> to <%= flight.destination %>
               </div>
-            </li>
-          <% end %>
+            </div>
+            <div class="second-line">
+              <div class="departs">
+                Departs: <%= flight.departure_time %>
+              </div>
+              <div class="arrives">
+                Arrives: <%= flight.arrival_time %>
+              </div>
+            </div>
+          </li>
         </ul>
       </div>
     </div>
     """
+  end
+
+  def handle_event("suggest", %{"airport" => prefix}, socket) do
+    matches = Airports.suggest(prefix)
+
+    {:noreply, assign(socket, matches: matches)}
+  end
+
+  def handle_event("search", %{"airport" => airport}, socket) do
+    send(self(), {:run_search, airport})
+
+    socket =
+      assign(socket,
+        airport: airport,
+        flights: [],
+        loading: true
+      )
+
+    {:noreply, socket}
+  end
+
+  def handle_info({:run_search, airport}, socket) do
+    socket =
+      assign(socket,
+        flights: Flights.search_by_airport(airport),
+        loading: false
+      )
+
+    {:noreply, socket}
   end
 end
